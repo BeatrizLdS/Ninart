922e9afe285522f6348ac32624ad9153597206d4
BeatrizLdS <beatriz_.silva@hotmail.com>
2023-03-02 17:40:58 -0300

    :recycle: descomentando algumas partes

--- ninart_app/Controllers/CollectionDataSource.swift
+++ ninart_app/Controllers/CollectionDataSource.swift
@@ -7,35 +7,35 @@
 
 import UIKit
 
-// let story = Bundle.main.decode([Story].self, from: "data.json")!
+ let story = Bundle.main.decode([Story].self, from: "data.json")!
 
-// class CollectionDataSource: NSObject, UICollectionViewDataSource {
-//    let images : [String] = {
-//        var images : [String] = []
-//        for pages in story {
-//            images.append(pages.image)
-//        }
-//        return images
-//    }()
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return images.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView,
-//                        cellForItemAt indexPath: IndexPath
-//    ) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
-//                                                      for: indexPath) as! CollectionViewCell
-//        let imageView: UIImageView = {
-//           let imageView = UIImageView()
-//            imageView.image = UIImage(named: images[indexPath.item])
-//            imageView.contentMode = .scaleToFill
-//            return imageView
-//        }()
-//        cell.addSubview(imageView)
-//        imageView.frame = cell.bounds
-//        return cell
-//    }
-//
-// }
+ class CollectionDataSource: NSObject, UICollectionViewDataSource {
+    let images : [String] = {
+        var images : [String] = []
+        for pages in story {
+            images.append(pages.image)
+        }
+        return images
+    }()
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                                      for: indexPath) as! CollectionViewCell
+        let imageView: UIImageView = {
+           let imageView = UIImageView()
+            imageView.image = UIImage(named: images[indexPath.item])
+            imageView.contentMode = .scaleToFill
+            return imageView
+        }()
+        cell.addSubview(imageView)
+        imageView.frame = cell.bounds
+        return cell
+    }
+
+ }
